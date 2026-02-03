b8f1dd9d3a428c8943e8a092cc7d1835280b6eaa
Ray Elder <hello@rayelder.com>
2026-02-03 13:26:16 -0700

    Add custom transform to css properties

--- build-output.js
+++ build-output.js
@@ -1,9 +1,23 @@
 import { register } from "@tokens-studio/sd-transforms";
 import StyleDictionary from "style-dictionary";
 
-// will register them on StyleDictionary object
-// that is installed as a dependency of this package.
 register(StyleDictionary);
+
+StyleDictionary.registerTransform({
+  name: "name/iris",
+  type: "name",
+  transform: (token, options) => {
+    const name = token.path
+      .filter(
+        (part) => !["Primitives/Light", "Primitives", "Light"].includes(part),
+      )
+      .join("-")
+      .toLowerCase()
+      .replace(/\//g, "-");
+
+    return options.prefix ? `${options.prefix}-${name}` : name;
+  },
+});
 
 const sd = new StyleDictionary({
   source: ["tokens/**/*.json"],
@@ -12,13 +26,11 @@ const sd = new StyleDictionary({
     css: {
       transformGroup: "tokens-studio",
       prefix: "iris-color",
-      transforms: ["name/kebab", "color/rgb"],
+      transforms: ["name/iris", "color/rgb"],
+
       buildPath: "build/css/",
       files: [
-        {
-          destination: "iris-design-tokens.css",
-          format: "css/variables",
-        },
+        { destination: "iris-design-tokens.css", format: "css/variables" },
       ],
     },
   },

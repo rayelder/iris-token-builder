bf2bef7b595578ab344f769a258711b1d42be8e3
Ray Elder <hello@rayelder.com>
2026-02-03 09:14:48 -0700

    Focus on CSS only

--- build-output.js
+++ build-output.js
@@ -7,88 +7,17 @@ register(StyleDictionary);
 
 const sd = new StyleDictionary({
   source: ["tokens/**/*.json"],
-  preprocessors: ["tokens-studio"], // <-- since 0.16.0 this must be explicit
+  preprocessors: ["tokens-studio"],
   platforms: {
-    js: {
-      transformGroup: "tokens-studio", // <-- apply the tokens-studio transformGroup to apply all transforms
-      prefix: "iris-color",
-      transforms: ["name/kebab", "color/rgb"], // Use the custom transform
-      buildPath: "build/js/",
-      files: [
-        {
-          format: "javascript/esm",
-          destination: "colors.js",
-          options: {
-            minify: true,
-          },
-        },
-      ],
-    },
     css: {
-      transformGroup: "tokens-studio", // <-- apply the tokens-studio transformGroup to apply all transforms
+      transformGroup: "tokens-studio",
       prefix: "iris-color",
-      transforms: ["name/kebab", "color/rgb"], // Use the custom transform
+      transforms: ["name/kebab", "color/rgb"],
       buildPath: "build/css/",
       files: [
         {
           destination: "iris-design-tokens.css",
           format: "css/variables",
-        },
-      ],
-    },
-    android: {
-      transformGroup: "android",
-      buildPath: "build/android/",
-      files: [
-        {
-          destination: "colors.xml",
-          format: "android/colors",
-        },
-      ],
-    },
-    "ios-swift": {
-      transformGroup: "ios-swift",
-      buildPath: "build/ios-swift/",
-      files: [
-        {
-          destination: "StyleDictionary+Class.swift",
-          format: "ios-swift/class.swift",
-          options: {
-            className: "StyleDictionaryClass",
-          },
-        },
-        {
-          destination: "StyleDictionary+Enum.swift",
-          format: "ios-swift/enum.swift",
-          options: {
-            className: "StyleDictionaryEnum",
-          },
-        },
-        {
-          destination: "StyleDictionary+Struct.swift",
-          format: "ios-swift/any.swift",
-          options: {
-            className: "StyleDictionaryStruct",
-            imports: "SwiftUI",
-            objectType: "struct",
-            accessControl: "internal",
-          },
-        },
-      ],
-    },
-    "ios-swift-separate-enums": {
-      transformGroup: "ios-swift-separate",
-      buildPath: "build/ios-swift/",
-      files: [
-        {
-          destination: "StyleDictionaryColor.swift",
-          format: "ios-swift/enum.swift",
-          options: {
-            className: "StyleDictionaryColor",
-          },
-          filter: {
-            type: "color",
-          },
         },
       ],
     },

66dc0098ebbebfaf39f73716cc6618f3e2424a42
Ray Elder <hello@rayelder.com>
2026-02-03 08:20:51 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Iris Design System
+
+## Token Builder
+
+```
+git pull
+npm run create
+```
+
+Last update: 2/3/26
---
+++ build/ios-swift/StyleDictionary+Enum.swift
@@ -0,0 +1,59 @@
+
+//
+// StyleDictionary+Enum.swift
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+import UIKit
+
+public enum StyleDictionaryEnum {
+    public static let green100 = UIColor(red: 0.965, green: 0.992, blue: 0.988, alpha: 1)
+    public static let green200 = UIColor(red: 0.855, green: 0.965, blue: 0.949, alpha: 1)
+    public static let green300 = UIColor(red: 0.663, green: 0.918, blue: 0.882, alpha: 1)
+    public static let green400 = UIColor(red: 0.322, green: 0.851, blue: 0.792, alpha: 1)
+    public static let green500 = UIColor(red: 0.000, green: 0.757, blue: 0.678, alpha: 1)
+    public static let green600 = UIColor(red: 0.000, green: 0.627, blue: 0.545, alpha: 1)
+    public static let green700 = UIColor(red: 0.000, green: 0.459, blue: 0.396, alpha: 1)
+    public static let green800 = UIColor(red: 0.000, green: 0.337, blue: 0.294, alpha: 1)
+    public static let green900 = UIColor(red: 0.000, green: 0.204, blue: 0.188, alpha: 1)
+    public static let indigo100 = UIColor(red: 0.957, green: 0.988, blue: 1.000, alpha: 1)
+    public static let indigo200 = UIColor(red: 0.871, green: 0.945, blue: 0.980, alpha: 1)
+    public static let indigo300 = UIColor(red: 0.729, green: 0.867, blue: 0.941, alpha: 1)
+    public static let indigo400 = UIColor(red: 0.541, green: 0.753, blue: 0.890, alpha: 1)
+    public static let indigo500 = UIColor(red: 0.314, green: 0.600, blue: 0.827, alpha: 1)
+    public static let indigo600 = UIColor(red: 0.000, green: 0.400, blue: 0.745, alpha: 1)
+    public static let indigo700 = UIColor(red: 0.000, green: 0.075, blue: 0.635, alpha: 1)
+    public static let indigo800 = UIColor(red: 0.000, green: 0.000, blue: 0.514, alpha: 1)
+    public static let indigo900 = UIColor(red: 0.051, green: 0.000, blue: 0.361, alpha: 1)
+    public static let neutral100 = UIColor(red: 0.976, green: 0.988, blue: 1.000, alpha: 1)
+    public static let neutral200 = UIColor(red: 0.933, green: 0.949, blue: 0.976, alpha: 1)
+    public static let neutral300 = UIColor(red: 0.867, green: 0.882, blue: 0.918, alpha: 1)
+    public static let neutral400 = UIColor(red: 0.773, green: 0.796, blue: 0.839, alpha: 1)
+    public static let neutral500 = UIColor(red: 0.655, green: 0.682, blue: 0.737, alpha: 1)
+    public static let neutral600 = UIColor(red: 0.518, green: 0.549, blue: 0.616, alpha: 1)
+    public static let neutral700 = UIColor(red: 0.361, green: 0.400, blue: 0.478, alpha: 1)
+    public static let neutral800 = UIColor(red: 0.196, green: 0.239, blue: 0.329, alpha: 1)
+    public static let neutral900 = UIColor(red: 0.031, green: 0.071, blue: 0.169, alpha: 1)
+    public static let orange100 = UIColor(red: 1.000, green: 0.984, blue: 0.596, alpha: 1)
+    public static let orange200 = UIColor(red: 1.000, green: 0.937, blue: 0.525, alpha: 1)
+    public static let orange300 = UIColor(red: 1.000, green: 0.859, blue: 0.400, alpha: 1)
+    public static let orange400 = UIColor(red: 1.000, green: 0.745, blue: 0.192, alpha: 1)
+    public static let orange500 = UIColor(red: 1.000, green: 0.588, blue: 0.000, alpha: 1)
+    public static let orange600 = UIColor(red: 0.953, green: 0.420, blue: 0.000, alpha: 1)
+    public static let orange700 = UIColor(red: 0.910, green: 0.263, blue: 0.000, alpha: 1)
+    public static let orange800 = UIColor(red: 0.875, green: 0.078, blue: 0.000, alpha: 1)
+    public static let orange900 = UIColor(red: 0.851, green: 0.000, blue: 0.000, alpha: 1)
+    public static let pureBlack = UIColor(red: 0.086, green: 0.086, blue: 0.086, alpha: 1)
+    public static let pureWhite = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let red100 = UIColor(red: 1.000, green: 0.980, blue: 0.973, alpha: 1)
+    public static let red200 = UIColor(red: 1.000, green: 0.918, blue: 0.894, alpha: 1)
+    public static let red300 = UIColor(red: 0.992, green: 0.816, blue: 0.765, alpha: 1)
+    public static let red400 = UIColor(red: 0.969, green: 0.667, blue: 0.588, alpha: 1)
+    public static let red500 = UIColor(red: 0.925, green: 0.467, blue: 0.373, alpha: 1)
+    public static let red600 = UIColor(red: 0.851, green: 0.137, blue: 0.047, alpha: 1)
+    public static let red700 = UIColor(red: 0.725, green: 0.000, blue: 0.000, alpha: 1)
+    public static let red800 = UIColor(red: 0.545, green: 0.000, blue: 0.000, alpha: 1)
+    public static let red900 = UIColor(red: 0.333, green: 0.000, blue: 0.000, alpha: 1)
+}
